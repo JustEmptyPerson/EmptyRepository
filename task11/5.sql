53b676798e5a69c291a9cb39913791614171cdfe
Roman <Rrom4eg6@gmail.com>
2021-05-25 18:49:46 +0300

    added task11

---
+++ task11/5.sql
@@ -0,0 +1,6 @@
+#CREATE TABLE posts ( id INT, discount INT, description VARCHAR(128),author VARCHAR(128), photoLink VARCHAR(128), createdAt DATE, likes VARCHAR(128),  hashtags VARCHAR(128));
+Use dr;
+#SELECT*FROM posts where createdAt=2021-03-01;
+SELECT name,description, createdAt as date
+FROM posts inner join users on users.id=user
+ORDER BY createdAt ASC
